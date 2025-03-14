d44172ad2181f2312affce748bff20bb5a0c48d8
JFH000 <jf.hernandezp1uniandes.edu.co>
2025-03-14 03:04:12 -0500

    resultado update

--- EpilepsIA/Resultado/models.py
+++ EpilepsIA/Resultado/models.py
@@ -1,11 +1,13 @@
 import uuid
 from django.db import models
 
+from Examen.models import Examen
+
 class Resultado(models.Model):
-    # Using UUIDField for a globally unique identifier
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     fecha = models.DateTimeField(auto_now_add=True)
     respuesta = models.TextField()
-
+    examen = models.OneToOneField(Examen, on_delete=models.CASCADE, null=True, blank=True)
+    
     def __str__(self):
         return str(self.id)
