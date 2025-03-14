53cb254bd37926d0fafed22ff41047cc5b840c93
JFH000 <jf.hernandezp1uniandes.edu.co>
2025-03-14 03:04:04 -0500

    examen update

--- EpilepsIA/Examen/models.py
+++ EpilepsIA/Examen/models.py
@@ -1,28 +1,30 @@
 import uuid
 from django.db import models
 
+from Solicitud.models import Solicitud
+from Paciente.models import Paciente
+
 class Examen(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     fecha = models.DateTimeField(auto_now_add=True)
     estado = models.TextField()
-    #resultado = models.OneToOneField(Resultado, on_delete=models.CASCADE)
-    #solicitud = models.OneToOneField(Solicitud, on_delete=models.CASCADE)
-    #paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
+    solicitud = models.OneToOneField(Solicitud, on_delete=models.CASCADE, null=True)
+    paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE, null=True)
     
     def __str__(self):
         return self.id
 
 class miRNA(Examen):
-    resultado = models.JSONField(null=True, blank=True)
+    resultado_miRNA = models.JSONField(null=True, blank=True)
     def __str__(self):
         return self.id
     
 class MRI(Examen):
-    resultado = models.JSONField(null=True, blank=True)
+    resultado_MRI = models.JSONField(null=True, blank=True)
     def __str__(self):
         return self.id
     
 class EEG(Examen):
-    resultado = models.JSONField(null=True, blank=True)
+    resultado_eeg = models.JSONField(null=True, blank=True)
     def __str__(self):
         return self.id
